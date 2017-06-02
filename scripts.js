c483bbdd3e92fc6346fb2c645e366e303a2d98d7
elliot richardson <ear78@aol.com>
2017-06-02 11:51:50 -0500

    finalized functionality and created some animations along with a modal

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
+"use strict";
 /* find all of the words that start with the letter 't' and return total.
 find all of the words that end with the letter 'e' and return total.
-find all of the words that start and end with the letter 't' and 'e'.
+find all of the words that start and end with the letter 't' and 'e' return total.
 */
 
 var preamble = 'We the People of the United States, in Order to form a more perfect Union, establish Justice, insure domestic Tranquility, provide for the common defence, promote the general Welfare, and secure the Blessings of Liberty to ourselves and our Posterity, do ordain and establish this Constitution for the United States of America.';
@@ -10,7 +11,7 @@ function firstLast() {
   var e = 0; // initialize e total
   var both = 0; // initialize both total
 
-  // arg1 split to array of substrings and set to lowercase
+  // split to array of substrings and set to lowercase
   var splitStr = preamble.toLowerCase().split(" ");
 
   for(var i = 0; i < splitStr.length; i++){
@@ -28,12 +29,30 @@ function firstLast() {
     }
    }
 
+
    // total up all forms of the counts
-   var total = "<div><span class='letters'>T : </span>" + t + "</div>" + "<div><span class='letters'> E : </span>" + e + "</div>" + "<span class='letters'>T & E : </span>" + both;
+   var total = "<div class='total-containers'><span class='letters'>T : </span>" + t + "</div>" + "<div class='total-containers'><span class='letters'> E : </span>" + e + "</div>" + "<div class='total-containers'><span class='letters'>T & E : </span>" + both + "</div>";
 
    // get element on page to print to page
    var print = document.getElementById('finalCount');
 
-  return print.innerHTML = total;
+   // RETURN STATEMENT
+   return print.innerHTML = total;
 
 }// END
+
+firstLast();
+
+
+/////////// ONCLICK EVENT TO SHOW MODAL DATA ////////////////
+
+var getModal = document.getElementById('modal');
+
+var showModal = function(){
+    getModal.style.transform = "translateY(0)";
+}
+
+var closeModal = function(){
+    getModal.style.transition = "all .7s ease";
+    getModal.style.transform = "translateY(-200vh)";
+}
